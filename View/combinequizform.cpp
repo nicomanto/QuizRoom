11714d3fc02a03716c3ec7db937a7899aef9482c
Niccolò Mantovani <nicomanto49@gmail.com>
2020-05-10 14:11:38 +0200

    Form dei quiz da combinare le parole

--- View/combinequizform.cpp
+++ View/combinequizform.cpp
@@ -1,6 +1,56 @@
 #include "combinequizform.h"
 
-CombineQuizForm::CombineQuizForm()
-{
+CombineQuizForm::CombineQuizForm(QWidget *parent): QuizBaseForm(parent){
+    main_layout= new QVBoxLayout(this);
+
+    question=new QLabel("Famiglia dell'animale?",this);
+    container_answers= new QGroupBox(this);
+    answers_layout= new QGridLayout(container_answers);
+
+    answers.push_back(new QComboBox(this));
+    answers.push_back(new QComboBox(this));
+    answers.push_back(new QComboBox(this));
+    answers.push_back(new QComboBox(this));
+
+    elements.push_back(new QLabel("Cavallo",this));
+    elements.push_back(new QLabel("Lucertola",this));
+    elements.push_back(new QLabel("Delfino",this));
+    elements.push_back(new QLabel("Pesce pagliaccio",this));
+
+
+    for(unsigned int i=0; i<4;++i){
+        answers[i]->addItem("mammifero");
+        answers[i]->addItem("rettile");
+        answers[i]->addItem("presce");
+    }
+
+
+
+    addForm();
+
+    setStyle();
+
+    setLayout(main_layout);
+}
+
+
+void CombineQuizForm::addForm(){
+    main_layout->addWidget(question);
+
+
+
+
+    for(unsigned int i=0; i<4;++i){
+
+            answers_layout->addWidget(elements[i],i,0,Qt::AlignCenter);
+
+
+           answers_layout->addWidget(answers[i],i,1,Qt::AlignCenter);
+    }
+
+    answers_layout->setSpacing(20);
+    main_layout->addWidget(container_answers);
 
 }
+
+
--- View/combinequizform.h
+++ View/combinequizform.h
@@ -1,11 +1,30 @@
 #ifndef COMBINEQUIZFORM_H
 #define COMBINEQUIZFORM_H
 
+#include <QFile>
+#include <QComboBox>
 
-class CombineQuizForm : public BaseForm
-{
+#include "Model/myvector.h"
+#include "quizbaseform.h"
+
+
+class CombineQuizForm : public QuizBaseForm{
+    Q_OBJECT
+private:
+
+   MyVector<QLabel*> elements;
+   MyVector<QComboBox*> answers;
+
 public:
-    CombineQuizForm();
+   explicit CombineQuizForm(QWidget *parent = nullptr);
+   ~CombineQuizForm()= default;
+
+   virtual void addForm();
+
+
+signals:
+
+public slots:
 };
 
 #endif // COMBINEQUIZFORM_H
