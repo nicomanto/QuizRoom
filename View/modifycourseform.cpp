5339ce2e0aaafd75bd34f3419b9be4cf4e781540
Niccolò Mantovani <nicomanto49@gmail.com>
2020-05-12 12:07:00 +0200

    Aggiunto css form di modifica del corso

---
+++ View/modifycourseform.cpp
@@ -0,0 +1,6 @@
+#include "modifycourseform.h"
+
+ModifyCourseForm::ModifyCourseForm()
+{
+
+}
---
+++ View/modifycourseform.h
@@ -0,0 +1,11 @@
+#ifndef MODIFYCOURSEFORM_H
+#define MODIFYCOURSEFORM_H
+
+
+class ModifyCourseForm : public BaseForm
+{
+public:
+    ModifyCourseForm();
+};
+
+#endif // MODIFYCOURSEFORM_H
