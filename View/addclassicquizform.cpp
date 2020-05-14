e9780eba4766939d91e8f30ba26f2b94872f96a5
Niccolò Mantovani <nicomanto49@gmail.com>
2020-05-14 12:26:25 +0200

    Aggiunto widget per l'aggiunta di un classico quiz

--- View/addclassicquizform.cpp
+++ View/addclassicquizform.cpp
@@ -1,6 +1,61 @@
 #include "addclassicquizform.h"
 
-AddClassicQuizForm::AddClassicQuizForm()
-{
+AddClassicQuizForm::AddClassicQuizForm(QWidget *parent): AddQuizForm(parent){
+
+    main_layout=new QVBoxLayout(this);
+
+    for(unsigned int i=0; i<4; ++i){
+        check_answers.push_back(new QCheckBox(this));
+        answers_form.push_back(new QLineEdit(this));
+    }
+
+
+
+    addForm();
+
+    setStyle();
+
+    setLayout(main_layout);
 
 }
+
+
+void AddClassicQuizForm::addForm(){
+
+    main_layout->addWidget(box_question);
+    main_layout->addWidget(box_answer);
+
+     layout_box_question->addWidget(question_form);
+
+    int j=0,k=0;
+    for(unsigned int i=0; i<4;++i){
+
+        if(i%2==0){
+            j++;
+            k=0;
+        }
+
+        QWidget* container_answer=new QWidget(this);
+        QHBoxLayout* layout_container_answers=new QHBoxLayout(container_answer);
+        layout_container_answers->addWidget(check_answers[i]);
+        layout_container_answers->addWidget(answers_form[i]);
+
+        layout_container_answers->setSpacing(5);
+
+        layout_box_answer->addWidget(container_answer,j,k);
+        k++;
+    }
+
+
+
+}
+
+void AddClassicQuizForm::setStyle(){
+    AddQuizForm::setStyle();
+
+    for(unsigned int i=0; i<4; ++i){
+        answers_form[i]->setPlaceholderText("Pinna la risposta se è corretta");
+    }
+}
+
+
--- View/addclassicquizform.h
+++ View/addclassicquizform.h
@@ -1,11 +1,31 @@
 #ifndef ADDCLASSICQUIZFORM_H
 #define ADDCLASSICQUIZFORM_H
 
+#include<QCheckBox>
+#include<QFile>
 
-class AddClassicQuizForm : public AddQuizForm
-{
+#include "addquizform.h"
+#include "Model/myvector.h"
+
+
+class AddClassicQuizForm : public AddQuizForm{
+    Q_OBJECT
+private:
+
+    MyVector<QCheckBox*> check_answers; //spunta per definire se la risposta è una riposta corretta
+    MyVector<QLineEdit*> answers_form; //risposta
+
+    virtual void addForm();
+
+    virtual void setStyle();
+
 public:
-    AddClassicQuizForm();
+    explicit AddClassicQuizForm(QWidget *parent = nullptr);
+    ~AddClassicQuizForm()=default;
+
+signals:
+
+public slots:
 };
 
 #endif // ADDCLASSICQUIZFORM_H
