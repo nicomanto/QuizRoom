1f6003e574b80e272f438b3abfb19bcac99cd1cc
Niccolò Mantovani <nicomanto49@gmail.com>
2020-05-15 19:31:11 +0200

    add form per aggiungere quiz

---
+++ View/containeraddquizform.cpp
@@ -0,0 +1,103 @@
+#include "containeraddquizform.h"
+
+ContainerAddQuizForm::ContainerAddQuizForm(QWidget *parent): PrincipalForm(parent),container_quiz(new QWidget(this)), layout_container_quiz(new QVBoxLayout(container_quiz)), scroll_layout(new QVBoxLayout(container_scroll)),end(new QPushButton("termina",this)), add_combine(new QPushButton("Quiz a combinazione",this)),
+    add_classic(new QPushButton("Quiz classico",this)),container_button(new QWidget(this)), layout_button(new QHBoxLayout(container_button)){
+
+    main_layout=new QVBoxLayout(this);
+    menubar=new QMenuBar(this);
+    scroll= new QScrollArea(this);
+
+    addMenu();
+    addForm();
+    setStyle();
+
+    setLayout(main_layout);
+}
+
+void ContainerAddQuizForm::addMenu(){
+
+    QAction* previous_page = new QAction("<-",menubar); //ritorno alla pagina precedent
+
+    menubar->addAction(previous_page);
+
+    main_layout->addWidget(menubar);
+
+    //connect della previous_page
+    connect(previous_page, SIGNAL(triggered()),this,SLOT(previous_page()));
+}
+
+void ContainerAddQuizForm::addForm(){
+
+   main_layout->addWidget(scroll);
+
+   scroll->setWidget( container_scroll );
+
+   scroll_layout->addWidget(container_quiz);
+
+   scroll_layout->addWidget(container_button);
+
+   layout_button->addWidget(end);
+   layout_button->addWidget(add_combine);
+   layout_button->addWidget(add_classic);
+
+   connect(add_classic,SIGNAL(clicked()),this,SLOT(addClassicWidget()));
+   connect(add_combine,SIGNAL(clicked()),this,SLOT(addCombineWidget()));
+   //connect(end,SIGNAL(clicked()),this,SLOT(close()));
+
+
+
+}
+
+void ContainerAddQuizForm::setStyle(){
+    PrincipalForm::setStyle();
+
+
+
+
+    scroll->setWidgetResizable(true);
+
+
+    //stile menubar
+    menubar->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Fixed);
+
+    //stile layout scroll
+    scroll_layout->setAlignment(Qt::AlignCenter);
+    scroll_layout->setSpacing(10);
+
+    //stile layout del button
+    layout_button->setAlignment(Qt::AlignCenter);
+
+
+    //setto la dimensione minima della finistra
+    setMinimumSize(QSize(width_screen/2,height_screen/2));
+
+    //aggiunta foglio di stile
+    QFile file(":/Resources/style_containerquiz.css");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(file.readAll());
+
+    setStyleSheet(styleSheet);
+}
+
+
+
+
+//SLOTS
+void ContainerAddQuizForm::addCombineWidget(){
+    quiz.push_back(new AddCombineQuizForm(quiz.size()+1,container_quiz));
+    layout_container_quiz->addWidget(quiz.back());
+}
+
+void ContainerAddQuizForm::addClassicWidget(){
+    quiz.push_back(new AddClassicQuizForm(quiz.size()+1,container_quiz));
+    layout_container_quiz->addWidget(quiz.back());
+}
+
+void ContainerAddQuizForm::previous_page(){
+    HomeworkForm* homework= new HomeworkForm();
+
+    homework->showMaximized();
+
+    close();
+}
+
---
+++ View/containeraddquizform.h
@@ -0,0 +1,51 @@
+#ifndef CONTAINERADDQUIZFORM_H
+#define CONTAINERADDQUIZFORM_H
+
+
+#include "principalform.h"
+#include "Model/myvector.h"
+#include "addcombinequizform.h"
+#include "addclassicquizform.h"
+#include "homeworkform.h"
+class ContainerAddQuizForm : public PrincipalForm{
+    Q_OBJECT
+private:
+
+    MyVector<AddQuizForm*> quiz; //vettore della vista dei quiz da aggiungere
+    QWidget* container_quiz;
+    QVBoxLayout* layout_container_quiz;
+
+    QVBoxLayout* scroll_layout;  //layout della scroll area
+
+    QPushButton* end; //bottone per terminare il l'aggiunta dei quiz
+    QPushButton* add_combine;  //aggiunge il form addcombinequizform
+    QPushButton* add_classic; //aggiunge il form addclassicquizform
+
+    QWidget* container_button; //contenitore dei
+    QHBoxLayout* layout_button; //layout del widget dei bottoni
+
+
+    virtual void addMenu();
+
+    virtual void addForm();
+
+    virtual void setStyle();
+
+
+public:
+    explicit ContainerAddQuizForm(QWidget *parent = nullptr);
+    ~ContainerAddQuizForm()= default;
+
+
+
+signals:
+
+public slots:
+private slots:
+    void addCombineWidget();
+    void addClassicWidget();
+
+    void previous_page();
+};
+
+#endif // CONTAINERADDQUIZFORM_H
