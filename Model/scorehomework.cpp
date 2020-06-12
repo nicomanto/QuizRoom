0b0e2e3212d22345fda33d9cd7e7f03cb3f9a420
Niccolò Mantovani <nicomanto49@gmail.com>
2020-06-12 15:03:03 +0200

    Rimozione campo dati total_score inutile

--- Model/scorehomework.cpp
+++ Model/scorehomework.cpp
@@ -2,23 +2,7 @@
 
 ScoreHomework::ScoreHomework(){}
 
-ScoreHomework::ScoreHomework(const string& t,const string& i): Homework(t,i), total_score(getTotalAllPoint()){}
-
-
-void ScoreHomework::addTotalScore(double p){
-    total_score+=p;
-}
-
-void ScoreHomework::removeTotalScore(double p){
-    total_score-=p;
-}
-
-
-
-double ScoreHomework::getTotalScore() const{
-    return total_score;
-}
-
+ScoreHomework::ScoreHomework(const string& t,const string& i): Homework(t,i){}
 
 bool ScoreHomework::isScoreHomework() const{
     return true;
@@ -30,18 +14,6 @@ bool ScoreHomework::isTimeHomework() const{
 
 bool ScoreHomework::haveResult() const{
     return true;
-}
-
-void ScoreHomework::addQuiz(Quiz* q){
-    Homework::addQuiz(q);
-
-    addTotalScore(getTotalAllPoint());
-}
-
-void ScoreHomework::removeQuiz(Quiz* q){
-    Homework::removeQuiz(q);
-
-    removeTotalScore(getTotalAllPoint());
 }
 
 double ScoreHomework::getResult() const{
--- Model/scorehomework.h
+++ Model/scorehomework.h
@@ -3,22 +3,13 @@
 #include "homework.h"
 
 class ScoreHomework : virtual public Homework{
-private:
-    double total_score;
-
-    void addTotalScore(double p);
-    void removeTotalScore(double p);
 public:
     ScoreHomework();
     ScoreHomework(const string& t,const string& i=string());
 
-    double getTotalScore() const;
     virtual bool isScoreHomework() const;
     virtual bool isTimeHomework() const;
     virtual bool haveResult() const;
-
-    virtual void addQuiz(Quiz* q); //ridefinizione perchè calcola il nuovo totale con l'aggiunta del container quiz
-    virtual void removeQuiz(Quiz* q); //ridefinizione perchè calcola il nuovo totale con la rimozione del container quiz
 
     virtual double getResult() const;   //ritorna la media del punteggio dei quiz
     virtual ScoreHomework *clone() const;
